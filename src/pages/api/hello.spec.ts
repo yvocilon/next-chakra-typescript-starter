42cfda88824cef6224ae845305113c10583e48a9
Yvo Cilon <yvo.cilon@appcore.nl>
2020-05-07 17:18:52 +0200

    add api route test

--- src/pages/api/hello.spec.ts
+++ src/pages/api/hello.spec.ts
@@ -1,8 +1,20 @@
-describe("hello", () => {
-    it("should return hello world", () => {
-        // TODO: figure out how to test api routes
-        expect(1).toEqual(1);
+import http from "http";
+import fetch from "isomorphic-unfetch";
+import listen from "test-listen";
+import { apiResolver } from "next/dist/next-server/server/api-utils";
+import handler from "./hello";
+import { NextApiRequest, NextApiResponse } from "next";
+
+describe("/hello", () => {
+    test("responds with hello world", async () => {
+        const requestHandler = (req: NextApiRequest, res: NextApiResponse) => {
+            return apiResolver(req, res, undefined, handler, undefined);
+        };
+        const server = http.createServer(requestHandler);
+        const url = await listen(server);
+        const response = await fetch(url);
+        expect(response.status).toBe(200);
+        expect(await response.json()).toStrictEqual({ message: "Hello World" });
+        return server.close();
     });
 });
-// for --isolatedFlags
-export {};
